e19a9635c88313a774a1ec7c95ebac92572e348f
MARudnicki <02979Maru>
2017-02-08 11:00:42 +0100

    Kernels continue

---
+++ src/main/java/naive/kernels/ExponentialKernel.java
@@ -0,0 +1,18 @@
+package naive.kernels;
+
+/**
+ * Created by Maciej Rudnicki on 06/02/2017.
+ */
+public class ExponentialKernel implements Kernel{
+
+    private final int power;
+
+    public ExponentialKernel(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public double predict(double probability) {
+        return Math.pow(probability,power);
+    }
+}
--- src/test/java/naive/ComponentFactory.java
+++ src/test/java/naive/ComponentFactory.java
@@ -1,7 +1,12 @@
 package naive;
 
+import naive.kernels.ExponentialKernel;
+import naive.kernels.RBFkernel;
 import naive.kernels.SVNkernel;
+import naive.preprocessors.IgnoreCasePreprocessor;
 import naive.preprocessors.RemoveExclationMarksPreprocessor;
+import naive.preprocessors.RemoveShortenThen2Preprocessor;
+import naive.preprocessors.RemoveSpecialCharsPreprocessor;
 
 /**
  * Created by Maciej Rudnicki on 06/02/2017.
@@ -10,15 +15,16 @@ public class ComponentFactory {
 
     public static Dataset getDataset(Class clazz) {
         return new Dataset.DatasetBuilder(clazz)
-                .with(new RemoveExclationMarksPreprocessor())
-//                .with(new RemoveQuotationPreprocessor())
-//                .with(new IgnoreCasePreprocessor())
+//                .with(new RemoveExclationMarksPreprocessor())
+                .with(new RemoveSpecialCharsPreprocessor())
                 .build();
     }
 
     public static NaiveBayesEngine getEngine(Dataset dataset) {
         return new NaiveBayesEngine(dataset)
-                .with(new SVNkernel());
+//                .with(new SVNkernel());
+                .with(new ExponentialKernel(3));
+//                .with(new RBFkernel());
 
     }
 
